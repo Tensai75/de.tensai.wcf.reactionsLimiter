f921b922a1bf52089396eeca3987c06c0335db7c
Tensai <tensai@gmx.net>
2023-04-16 15:22:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# de.tensai.wcf.reactionsLimiter
+Limits the allowed reactions for user groups within the last 24 h, 7 days and 30 days.
+Includes English and German language.
+
+## ACP (Users -> User Groups -> Edit User Group -> General Permissions -> Messages)
+![ACP](https://github.com/Tensai75/de.tensai.wcf.reactionsLimiter/raw/main/resources/acp.jpg)
+
+## Error message when the limit is reached
+![Error_Message](https://github.com/Tensai75/de.tensai.wcf.reactionsLimiter/raw/main/resources/error_message.jpg)
+
+### Minimum requirements
+Woltlab Suite v5.2
+
+### Limitations
+The reaction limiter does count all reactions regardless of the likeable object type.
---
+++ src/files/lib/system/event/listener/ReactionLimitertListener.class.php
@@ -0,0 +1,60 @@
+<?php
+namespace wcf\system\event\listener;
+
+use wcf\system\WCF;
+use wcf\system\exception\NamedUserException;
+
+/**
+ * @author 	Tensai
+ * @package	de.tensai.wcf.reactionsLimiter
+ */
+class ReactionLimitertListener implements IParameterizedEventListener {
+
+	/**
+	 * @see	wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName, array &$parameters) {
+
+		$day = 60 * 60 * 24;
+		$week = $day * 7;
+		$month = $day * 30;
+
+		if ($className == 'wcf\data\reaction\ReactionAction' && $eventName == 'validateAction') {
+			
+			$maxLikesPerDay = intval(WCF::getSession()->getPermission('user.like.maxLikesPerDay'));
+			if ($maxLikesPerDay > 0) {
+				$sql = "SELECT COUNT(likeID) FROM wcf".WCF_N."_like WHERE userID=? && time > ?";
+				$statement = WCF::getDB()->prepareStatement($sql);
+				$statement->execute([WCF::getUser()->userID, time()-$day]);
+				$row = $statement->fetchArray();
+				if ($row['COUNT(likeID)'] >= $maxLikesPerDay) {
+					throw new NamedUserException(WCF::getLanguage()->getDynamicVariable('wcf.like.maxLikesPerDayReached'));
+				}
+			}
+			
+			$maxLikesPerWeek = intval(WCF::getSession()->getPermission('user.like.maxLikesPerWeek'));
+			if ($maxLikesPerWeek > 0) {
+				$sql = "SELECT COUNT(likeID) FROM wcf".WCF_N."_like WHERE userID=? && time > ?";
+				$statement = WCF::getDB()->prepareStatement($sql);
+				$statement->execute([WCF::getUser()->userID, time()-$week]);
+				$row = $statement->fetchArray();
+				if ($row['COUNT(likeID)'] >= $maxLikesPerWeek) {
+					throw new NamedUserException(WCF::getLanguage()->getDynamicVariable('wcf.like.maxLikesPerWeekReached'));
+				}
+			}
+			
+			$maxLikesPerMonth = intval(WCF::getSession()->getPermission('user.like.maxLikesPerMonth'));
+			if ($maxLikesPerMonth > 0) {
+				$sql = "SELECT COUNT(likeID) FROM wcf".WCF_N."_like WHERE userID=? && time > ?";
+				$statement = WCF::getDB()->prepareStatement($sql);
+				$statement->execute([WCF::getUser()->userID, time()-$month]);
+				$row = $statement->fetchArray();
+				if ($row['COUNT(likeID)'] >= $maxLikesPerMonth) {
+					throw new NamedUserException(WCF::getLanguage()->getDynamicVariable('wcf.like.maxLikesPerMonthReached'));
+				}
+			}
+			
+		}
+
+	}
+}
